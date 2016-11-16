fab31885b949e1aef807166e320145929d0942e8
Laura Kressin <laurakressin@gmail.com>
2016-11-16 10:32:38 -0600

    correcting project

---
+++ TimeToTalk.MN App/TimeToTalk.MN AppUITests/TimeToTalk_MN_AppUITests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeToTalk_MN_AppUITests.swift
+//  TimeToTalk.MN AppUITests
+//
+//  Created by Laura Kressin on 11/16/16.
+//  Copyright © 2016 TimeToTalk.MN. All rights reserved.
+//
+
+import XCTest
+
+class TimeToTalk_MN_AppUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
